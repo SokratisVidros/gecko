edb93b352b540a423ba777fa0a7f8b0e156fbdf7
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-30 16:50:26 +0300

    test

--- src/api.js
+++ src/api.js
@@ -2,13 +2,9 @@ const fetch = require('node-fetch');
 
 const BASE_URL = 'https://www.workable.com/api';
 
-// 'https://www.workable.com/api/accounts/6504'
-// 'https://www.workable.com/api/jobs/9E072E09CD'
-
 function getJobByShortcode(shortcode) {
   return fetch(`${BASE_URL}/jobs/${shortcode}`)
     .then(response => {
-      // console.log('RESPONSE:', response);
       if (!response.ok) {
         new Error(response.statusText);
       }
@@ -20,14 +16,12 @@ function getJobByShortcode(shortcode) {
     });
 };
 
-exports.getJobs = function getJobs(shortcodes, onSuccess = () => {}, onFailure = () => {}) {
+exports.getJobs = function getJobs(shortcodes) {
   return Promise.all(shortcodes.map(getJobByShortcode))
     .then(jobs => {
-      // console.log('VALUES:', jobs);
       return jobs.filter(job => !!job);
     })
     .catch(err => {
-      // console.log('ERROR ALL', err);
-      onFailure(err);
+      return err;
     });
 };
--- src/parseConfig.js
+++ src/parseConfig.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 function parseConfig() {
   const packageJsonPath = `${process.cwd()}/package.json`;
-  console.log('---PATH:' + packageJsonPath);
   const content = fs.readFileSync(packageJsonPath);
   return JSON.parse(content).gecko;
 };
