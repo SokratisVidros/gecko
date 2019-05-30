12676dd70470abfc6b500c84cb28c70e2c32bd2f
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-30 13:06:19 +0300

    parse parent package.json

--- cli.js
+++ cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+var parent = require('parent-package-json');
+var parentJSON = parent().parse();
+
+console.log(parentJSON);
+
+if (!parentJSON) {
+  console.log('Couldn\'t find/parse package.json');
+}
+
+console.log('gecko config:', parentJSON.gecko);
+
 console.log('HELLO');
