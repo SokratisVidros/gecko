0e6b204f88fa86f850c24efff1c0ad49e683116e
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-30 13:40:58 +0300

    sdfd

--- cli.js
+++ cli.js
@@ -2,25 +2,9 @@
 
 const parseConfig = require('./src/parseConfig');
 
-const config = parseConfig();
-console.log('CONTENT', config);
-const carrersPageUrl = config.carrersPage;
-const jobs = config.jobs;
+const geckoConfig = parseConfig();
+const carrersPageUrl = geckoConfig.carrersPage;
+const jobs = geckoConfig.jobs;
 
-console.log(jobs);
-
-
-// // let  parent = require('parent-package-json');
-// console.log(process.cwd());
-// console.log(parent, parent())
-// // var parentJSON = parent().parse();
-
-// console.log(parentJSON);
-
-// if (!parentJSON) {
-//   console.log('Couldn\'t find/parse package.json');
-// }
-
-// console.log('gecko config:', parentJSON.gecko);
-
-// console.log('HELLO');
+console.log('carrers page:' + carrersPageUrl);
+console.log('jobs:', jobs);
--- src/parseConfig.js
+++ src/parseConfig.js
@@ -4,7 +4,7 @@ function parseConfig() {
   const packageJsonPath = `${process.cwd()}/package.json`;
   console.log('---PATH:' + packageJsonPath);
   const content = fs.readFileSync(packageJsonPath);
-  return JSON.parse(content);
+  return JSON.parse(content).gecko;
 };
 
 module.exports = parseConfig;
