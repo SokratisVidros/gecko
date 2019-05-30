6a676ee87a197d86efeac2e18611e94a4845dd90
tsirosg <tsiros@workable.com>
2019-05-30 12:29:30 +0300

    Fetch job data from url

---
+++ src/fetchData.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+
+fetch('https://www.workable.com/api/jobs/9E072E09CD')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log("Apply here: " + myJson.application_url);
+    console.log("View Job: " + myJson.shortlink);
+    console.log("Job shortcode: " + myJson.shortcode);
+  });
+
+
