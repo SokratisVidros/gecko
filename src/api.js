8fe5cb2763d3858128fd04161fddb915b55d3c98
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-30 15:10:07 +0300

    test

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const BASE_URL = 'https://www.workable.com/api';
 // 'https://www.workable.com/api/jobs/9E072E09CD'
 
 function getJobByShortcode(shortcode, onSuccess = () => {}, onFailure = () => {}) {
-  fetch(`${BASE_URL}/jobs/${shortcode}`)
+  return fetch(`${BASE_URL}/jobs/${shortcode}`)
     .then(response => {
       // console.log('RESPONSE:', response);
       if (!response.ok) {
@@ -15,18 +15,18 @@ function getJobByShortcode(shortcode, onSuccess = () => {}, onFailure = () => {}
       return response.json();
     })
     .then(data => {
-      // console.log('DATA:', data);
+      console.log('DATA:', data);
       onSuccess(data);
     })
     .catch(err => {
-      // console.log('ERROR:', err);
+      console.log('ERROR:', err);
       onFailure(err);
     });
 };
 
 exports.getJobs = function getJobs(shortcodes, onSuccess, onFailure) {
 
-  Promise.all(shortcodes.map(getJobByShortcode))
+  Promise.all(shortcodes.map(shortcode => getJobByShortcode(shortcode)))
     .then(values => console.log('VALUES:', values));
   // const promises = shortcodes.map(getJobByShortcode)
 
