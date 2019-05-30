e0eec72be4933026a919b08c4ad38bc852c69786
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-30 13:24:14 +0300

    asddsfasd

--- cli.js
+++ cli.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 
-var parent = require('parent-package-json');
-console.log(process.cwd());
-console.log(parent, parent())
-var parentJSON = parent().parse();
+let fs = require("fs");
 
-console.log(parentJSON);
+const packageJsonPath = `${process.cwd()}/package.json`;
+const content = fs.readFileSync(packageJsonPath);
 
-if (!parentJSON) {
-  console.log('Couldn\'t find/parse package.json');
-}
+console.log(content);
+console.log(content.gecko);
 
-console.log('gecko config:', parentJSON.gecko);
 
-console.log('HELLO');
+
+// // let  parent = require('parent-package-json');
+// console.log(process.cwd());
+// console.log(parent, parent())
+// // var parentJSON = parent().parse();
+
+// console.log(parentJSON);
+
+// if (!parentJSON) {
+//   console.log('Couldn\'t find/parse package.json');
+// }
+
+// console.log('gecko config:', parentJSON.gecko);
+
+// console.log('HELLO');
