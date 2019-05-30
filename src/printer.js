f79062b933034fd60be0005e7deb9f5a8186e345
Sokratis Vidros <sokratis.vidros@gmail.com>
2019-05-30 14:46:16 +0300

    fixup! Play with printer

--- src/printer.js
+++ src/printer.js
@@ -7,7 +7,7 @@ const line1 = chalk.blue('Hello') + ' World' + chalk.red('!')
 const line2 = chalk.green('I am a 🦎');
 // const line3 = chalkAnimation.karaoke('and I am green!').stop();
 
-line3.render();
+// line3.render();
 
 const content = [line1, line2].join('\n')
 console.log(boxen(content, { padding: 2, borderColor: 'yellow', borderStyle: 'round' }));
