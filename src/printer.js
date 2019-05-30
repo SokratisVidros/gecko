137173a5739926ae96257d4dd95669179010bc89
Sokratis Vidros <sokratis.vidros@gmail.com>
2019-05-30 14:45:50 +0300

    Play with printer

---
+++ src/printer.js
@@ -0,0 +1,13 @@
+const boxen = require('boxen');
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+
+// 🦎 === "\uD83E\uDD8E"
+const line1 = chalk.blue('Hello') + ' World' + chalk.red('!')
+const line2 = chalk.green('I am a 🦎');
+// const line3 = chalkAnimation.karaoke('and I am green!').stop();
+
+line3.render();
+
+const content = [line1, line2].join('\n')
+console.log(boxen(content, { padding: 2, borderColor: 'yellow', borderStyle: 'round' }));
