9b5831603a9aa8e7d4dee7f96da258a3cde9e5e2
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-31 15:22:10 +0300

    refactor utils

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,11 @@ exports.extractCompanyNameFromUrl = function (url = '') {
 exports.extractCareersPageFromUrl = function (url = '') {
   return url.split('com')[0] + 'com';
 }
+
+exports.clearConsole = function () {
+  return process.stdout.write('\033c');
+}
+
+exports.randomize = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
