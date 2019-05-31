eb203e30db6f1d959544baa54c479142c1ca4169
Sokratis Vidros <sokratis.vidros@gmail.com>
2019-05-31 10:56:58 +0300

    Add gecko animation

---
+++ src/animateGecko.js
@@ -0,0 +1,36 @@
+require('./array');
+const fs = require('fs');
+const animation = require('ascii-animation');
+const chalk = require('chalk');
+
+const gecko = fs
+  .readFileSync('./src/gecko.txt', 'utf8')
+  .split(/\r?\n/g)
+
+const [columns, lines] = process.stdout.getWindowSize();
+
+const mainFrame = Array(lines + gecko.length - 1).fill('').concat(gecko);
+
+const frames = Array(Math.ceil(mainFrame.length / 2)).fill('').map((_, i) => {
+  const hOffset = ' '.repeat(
+    Math.floor(columns / 2) - 15 +    // Center horizontally
+    Math.floor(Math.random() * 5) + 1 // Add some random horizontal offset for a more natural move
+  );
+
+  return mainFrame
+    .rotate(-i * 2)
+    .slice(gecko.length, -gecko.length)
+    .map(l => chalk.green(hOffset + l))
+    .join("\n");
+});
+
+let condition = true;
+let secondsPerFrame = 0.15;
+
+animation.secondsPerFrame(secondsPerFrame);
+animation.animate(frames, () => condition, () => {}).bold();
+
+// setTimeout(() => {
+//   condition = false
+//   console.log('DONE')
+// }, secondsPerFrame * frames.length * 1000);
---
+++ src/array.js
@@ -0,0 +1,10 @@
+Array.prototype.rotate = function rotate(n) {
+  if (!Array.isArray(this)) {
+    throw new TypeError(`Expected an array, got ${typeof this}`);
+  }
+
+  const x = this.slice();
+  const num = typeof n === 'number' ? n : 0;
+
+  return x.splice(-num % x.length).concat(x);
+}
