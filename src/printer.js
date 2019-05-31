4971e4a9f48bb5d54d1b741a4e1c696d39c0ae9a
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-31 15:10:30 +0300

    Implement printer and animation

--- src/printer.js
+++ src/printer.js
@@ -1,9 +1,59 @@
+const readline = require('readline');
 const chalk = require('chalk');
+const terminalLink = require('terminal-link');
 
-module.exports = function(data) {
-  const line1 = chalk.blue('Hello') + ' World' + chalk.red('!')
-  const line2 = chalk.green('I am a 🦎');
+const animateGecko = require('./animateGecko');
+const reset = require('../reset');
 
-  const content = [line1, line2].join('\n')
-  console.log(data);
+const extractCompanyNameFromUrl = require('./utils').extractCompanyNameFromUrl;
+const extractCareersPageFromUrl = require('./utils').extractCareersPageFromUrl;
+
+const makePrompt = ({ companyName }) => `
+Hi! Have you found us or have we found you? 🙂
+
+You must be good at what you do. ${companyName} is hiring and we're looking for talented developers like you.
+
+Are you interested? [Y|n]
+`;
+
+const makeAd = ({ companyName, positionLink, carrersPageLink, workableLink }) => `
+Apply now for ${companyName}'s next ${positionLink} or view all ${companyName}'s ${carrersPageLink}.
+Powered by ${workableLink}.
+`;
+
+function showPrompt() {
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+  });
+
+  return ({ shortlink }) => new Promise(resolve => rl.question(makePrompt(extractCompanyNameFromUrl(shortlink)), ans => {
+      rl.close();
+      resolve(ans);
+  }))
 }
+
+function showJobAd(job) {
+  const companyName = extractCompanyNameFromUrl(job.shortlink);
+  const positionLink = terminalLink(job.title, job.shortlink);
+  const carrersPageLink = terminalLink('jobs', extractCareersPageFromUrl(job.shortlink));
+  const workableLink = terminalLink('Workable', 'https://www.workable.com/');
+
+  return () => console.log(makeAd({
+    companyName,
+    positionLink,
+    carrersPageLink,
+    workableLink
+  }));
+}
+
+function print(data) {
+  return animateGecko()
+    .then(reset)
+    .then(showPrompt(data))
+    .then(promptAnswer => { /* show add or skip */})
+    .then(showJobAd(data))
+    // .then(waitForUserInput)
+}
+
+module.exports = print;
