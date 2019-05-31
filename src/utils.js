94e8d2a43c05c028f2bea36b0713e24da851abaf
Nikos Petridis <nick.petridis93@gmail.com>
2019-05-31 14:58:36 +0300

    asd

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 exports.extractCompanyNameFromUrl = function (url) {
-  url.slice(8).split('.workable')[0];
+  return url.slice(8).split('.workable')[0];
 }
 
 exports.extractCareersPageFromUrl = function (url) {
-  url.split('com')[0] + 'com';
+  return url.split('com')[0] + 'com';
 }
